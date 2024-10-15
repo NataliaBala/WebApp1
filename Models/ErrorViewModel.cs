c7f86d3e12a9f58be090b136b7e9a77b889cb507
Natalia Bała <natalia.bala@microsoft.wsei.edu.pl>
2024-10-15 22:03:57 +0200

    1

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace WebAplication1.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
